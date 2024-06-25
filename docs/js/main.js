1a3baaede9a7cdf3b5f1abf9b14ba081bf09352b
Laura Rodríguez <laurarguezfdez@gmail.com>
2024-06-25 18:31:54 +0200

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,12 @@
-console.log(">> Ready :)");
+const e=document.querySelector(".js_cards-container");let t=[];const r="https://via.placeholder.com/210x295/ffffff/555555/?text=Disney";function s(a){return a.imageUrl?`<article class="characters_card">
+                        <div class="card_img_container">
+                            <img class="card_img" src=${a.imageUrl} alt="">
+                        </div>
+                        <h2 class="card_text">${a.name}</h2>
+                     </article>`:`<article class="characters_card">
+                        <div class="card_img_container">
+                            <img class="card_img" src=${r} alt="">
+                        </div>
+                        <h2 class="card_text">${a.name}</h2>
+                     </article>`}function i(){let a="";for(const c of t)a+=s(c);e.innerHTML=a}fetch("//api.disneyapi.dev/character?pageSize=50").then(a=>a.json()).then(a=>{t=a.data,i()});
 //# sourceMappingURL=main.js.map
