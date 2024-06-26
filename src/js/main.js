b21561c2fc0e8cbf12f4c0071e587b40b8b24314
Laura Rodríguez <laurarguezfdez@gmail.com>
2024-06-26 23:19:58 +0200

    Functions and styles added

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,8 @@ const cardsContainer = document.querySelector('.js_cards-container');
 const favourtiesCardsContainer = document.querySelector('.js_favourites-container');
 const searchButton = document.querySelector('.js_searchButton');
 const characterInput = document.querySelector('.js_characterInput');
+const clearFavouritesButton = document.querySelector('.js_clearButton');
+const resetSearchButton = document.querySelector('.js_resetButton');
 
 // DATOS
 
@@ -15,30 +17,21 @@ const placeholderImgURL = 'https://via.placeholder.com/210x295/ffffff/555555/?te
 
 // FUNCIONES
 
-function createArticleForCharacter(oneCharacter) {
 
-    if (oneCharacter.imageUrl) {
-        const html = `<article class="characters_card js_characters_card" data-id="${oneCharacter._id}">
-                        <div class="card_img_container">
-                            <img class="card_img" src=${oneCharacter.imageUrl} alt="">
-                        </div>
-                        <h2 class="card_text">${oneCharacter.name}</h2>
-                     </article>`;
-        return html;
-    }
-    else {
-        const html = `<article class="characters_card js_characters_card" data-id="${oneCharacter._id}">
-                        <div class="card_img_container">
-                            <img class="card_img" src=${placeholderImgURL} alt="">
-                        </div>
-                        <h2 class="card_text">${oneCharacter.name}</h2>
-                     </article>`;
-        return html;
-    }
-    
+function createArticleForCharacter(oneCharacter) {
+    //Función para crear el HTML de cada tarjeta de personaje
+    const html = `<article class="characters_card js_characters_card" data-id="${oneCharacter._id}">
+                    <div class="card_img_container">
+                        <img class="card_img" src=${oneCharacter.imageUrl ? oneCharacter.imageUrl : placeholderImgURL} alt="">
+                    </div>
+                    <h2 class="card_text">${oneCharacter.name}</h2>
+                 </article>`;
+    return html;   
 }
 
+
 function renderCharacters() {
+    //Función para renderizar los personajes en la sección de personajes
     let html = '';
 
     for (const oneCharacter of data) {
@@ -47,14 +40,17 @@ function renderCharacters() {
 
     cardsContainer.innerHTML = html;
 
+    //Agregar event listener a cada tarjeta de personaje
     const allCharactersCards = document.querySelectorAll('.js_characters_card');
-
+    
     for (const eachCard of allCharactersCards){
         eachCard.addEventListener( 'click', handleClickCard );
-    }
+    };
 }
 
+
 function renderFavourites() {
+    //Función para renderizar los favoritos
     let html = '';
 
     for (const oneCharacter of favourites) {
@@ -64,10 +60,34 @@ function renderFavourites() {
     favourtiesCardsContainer.innerHTML = html;
 }
 
+function removeFavouriteBackgroundColor() {
+    //Función para eliminar el color de fondo para las tarjetas que lo tengan
+    const allCards = document.querySelectorAll('.js_characters_card');
+
+    for (const eachCard of allCards) {
+        eachCard.classList.remove('favourite');
+    }
+}
+
+function addFavouriteBackgroundColor() {
+    const allCards = document.querySelectorAll('.js_characters_card');
+
+        for (const eachCard of allCards) {
+            //debugger;
+            const cardId = parseInt(eachCard.dataset.id);
+            const isFav = favourites.find(fav => fav._id === cardId);
+
+             if (isFav) {
+                 eachCard.classList.add('favourite');
+             };
+        }
+}
+
 // FUNCIONES DE EVENTOS (HANDLER)
 
 function handleClickCard(ev) {
-    
+    //Función para manejar el click en una tarjeta de personaje (agregar/quitar favorito)
+
     const clickedCardElement = ev.currentTarget;
     console.log(clickedCardElement);
     clickedCardElement.classList.toggle('favourite');
@@ -80,15 +100,18 @@ function handleClickCard(ev) {
 
     if (clickedFavouriteIndex === -1) {
         favourites.push(clickedCard);
-        renderFavourites();
     }
     else {
         favourites.splice(clickedFavouriteIndex, 1);
-        renderFavourites();
     }
+
+    localStorage.setItem('favourites', JSON.stringify(favourites));
+    renderFavourites();
+    addFavouriteBackgroundColor();
 }
 
 function handleClickSearch(ev) {
+    //Función para manejar la búsqueda de personajes
     ev.preventDefault();
     const searchedCharacter = characterInput.value;
     console.log(searchedCharacter);
@@ -98,13 +121,41 @@ function handleClickSearch(ev) {
         .then (dataFromOtherFetch => {
             data = dataFromOtherFetch.data;        
 
-            renderCharacters();   
+            renderCharacters();
+            addFavouriteBackgroundColor();   
         });
 }
+
+function handleClickClearAllFavourites(ev) {
+    //Función para borrar todos los favoritos de su sección y del localStorage
+    favourites = [];
+    localStorage.removeItem('favourites');
+    renderFavourites();
+    removeFavouriteBackgroundColor();
+}
+
+function handleClickResetSearch(ev) {
+    //Función que resetea la búsqueda mostrando todas las tarjetas de nuevo
+    ev.preventDefault();
+    characterInput.value = '';
+    fetch ('//api.disneyapi.dev/character?pageSize=50')
+        .then(response => response.json())
+        .then(dataFromFetch => {
+            data = dataFromFetch.data;        
+
+
+        renderCharacters();
+        addFavouriteBackgroundColor();
+
+        });
+};
 
 // EVENTOS
 
 searchButton.addEventListener( 'click', handleClickSearch);
+clearFavouritesButton.addEventListener( 'click', handleClickClearAllFavourites);
+resetSearchButton.addEventListener( 'click', handleClickResetSearch);
+
 
 // CÓDIGO CUANDO CARGA LA PÁGINA
 
@@ -113,6 +164,15 @@ fetch ('//api.disneyapi.dev/character?pageSize=50')
     .then(dataFromFetch => {
         data = dataFromFetch.data;        
 
-    renderCharacters();   
+    renderCharacters();
+    addFavouriteBackgroundColor();   
     });
 
+const favouritesFromLocalStorage = JSON.parse(localStorage.getItem('favourites'));
+
+if (favouritesFromLocalStorage !== null) {
+    favourites = favouritesFromLocalStorage;
+
+    renderFavourites();
+    addFavouriteBackgroundColor();
+}
